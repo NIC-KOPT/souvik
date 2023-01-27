ce234c8bb6bee1c0e5d41f71cbda532e475db520
spalnic <souvikpal.nic@gmail.com>
2023-01-27 16:57:01 +0530

    Souvik commit Arch

--- src/main/java/com/nic/souvik/SouvikApplication.java
+++ src/main/java/com/nic/souvik/SouvikApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+@EnableTransactionManagement 
 @SpringBootApplication
 public class SouvikApplication extends SpringBootServletInitializer{
 
@@ -15,6 +18,10 @@ public class SouvikApplication extends SpringBootServletInitializer{
  
 	public static void main(String[] args) {
 		SpringApplication.run(SouvikApplication.class, args);
+	}
+	@Bean
+	RestTemplate restTemplate() {
+	    return new RestTemplate();
 	}
 
 }
